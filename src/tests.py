7dd85bf16e9895ee7ce4ba9cbd2b13c27223e69f
rsmichael <michael.lrs@gmail.com>
2024-04-07 18:25:21 -0500

    added tests and test input files

---
+++ src/tests.py
@@ -0,0 +1,26 @@
+import pytest
+import sys
+import os
+from pdbvangogh import api
+import tensorflow as tf
+import random
+import numpy as np
+
+np.random.seed(42)
+tf.random.set_seed(42)
+random.seed(42)
+
+def test_test_func():
+    assert(True)
+    assert(api.test_func())
+def test_pdbvangogh():
+    api.pdbvangogh(background_image = 'tests/in/pdx.png', 
+                   content_image = 'tests/in/2l1v.png', 
+                   style_image = 'tests/in/starry_night.png',
+                   save_prefix = 'tests/out/2l1v_pdx_starry_night_10_20',
+                   content_size=100,
+                   background_size = 200,
+                   steps_per_epoch = 50,
+                   epochs = 1
+                   )
+    assert(True)
